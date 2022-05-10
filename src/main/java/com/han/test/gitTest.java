420c49697797b6a19d146d5d00fbc91e84723aea
han <han@qq.com>
2022-05-10 15:59:59 +0800

    first commit

---
+++ src/main/java/com/han/test/gitTest.java
@@ -0,0 +1,12 @@
+package com.han.test;
+
+/**
+ * @Description
+ * @Author 寒江飞冰
+ * @Date 2022/5/10 15:55
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello. git");
+    }
+}
